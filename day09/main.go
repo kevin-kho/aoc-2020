ea6078b09f58b9b68c066131e46661bea8cec6b7
Kevin Ho <kevin.ho01@sap.com>
2026-09-23 11:51:29 -0700

    day09 part 1 complete brute force

--- day09/main.go
+++ day09/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"strconv"
 
@@ -24,7 +25,39 @@ func GetIntArr(data []byte) ([]int, error) {
 	return res, nil
 }
 
-func SolvePartOne(intArr []int) {
+func SolvePartOne(intArr []int, preamble int) int {
+	// pointers for last X digits
+	l := 0
+	r := preamble - 1
+
+	// pointer for current digit
+	i := preamble
+
+	for i < len(intArr) {
+
+		// even := intArr[i]%2 == 0
+		found := false
+
+		for x := l; x < r+1; x++ {
+			for y := x + 1; y < r+1; y++ {
+				a := intArr[x]
+				b := intArr[y]
+				if a+b == intArr[i] {
+					found = true
+				}
+			}
+		}
+
+		if !found {
+			return intArr[i]
+		}
+
+		l++
+		r++
+		i++
+	}
+
+	return -1
 
 }
 
@@ -36,10 +69,13 @@ func main() {
 	}
 	data = common.TrimNewLineSuffix(data)
 
+	// intArr is not guaranteed to have unique values
 	intArr, err := GetIntArr(data)
 	if err != nil {
 		log.Fatal(err)
 	}
-	SolvePartOne(intArr)
+	// res := SolvePartOne(intArr, 5)
+	res := SolvePartOne(intArr, 25)
+	fmt.Println(res)
 
 }
