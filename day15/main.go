79366eb9f0d5d5fc62ae3138f2fb03e20361628c
Kevin Ho <kevin.ho01@sap.com>
2026-09-25 15:38:10 -0700

    day15 part1 complete

---
+++ day15/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kevin-kho/aoc-utilities/common"
+)
+
+func SolvePartOne(arr []int) {
+
+	// key: int
+	// values: the last two occurances; treat it like a queue
+	lastSeen := make(map[int][]int)
+
+	// initialize values
+	for i, val := range arr {
+		lastSeen[val] = append(lastSeen[val], i+1)
+		fmt.Printf("turn: %v; speak %v\n", i+1, val)
+	}
+
+	turn := len(arr) + 1
+	prev := arr[len(arr)-1]
+	for turn < 2021 {
+
+		// case: prev not seen, speak 0
+		if len(lastSeen[prev]) == 1 {
+			if len(lastSeen[0]) == 2 {
+				lastSeen[0] = lastSeen[0][1:]
+			}
+			lastSeen[0] = append(lastSeen[0], turn)
+			prev = 0
+		} else {
+			num := common.IntAbs(lastSeen[prev][0] - lastSeen[prev][1])
+			if len(lastSeen[num]) == 2 {
+				lastSeen[num] = lastSeen[num][1:]
+			}
+			lastSeen[num] = append(lastSeen[num], turn)
+			prev = num
+		}
+
+		fmt.Printf("turn: %v; speak %v\n", turn, prev)
+
+		turn++
+	}
+
+}
+
+func main() {
+	fmt.Println("hi")
+	// SolvePartOne([]int{0, 3, 6})
+	SolvePartOne([]int{6, 4, 12, 1, 20, 0, 16})
+}
